e0a2e06d46d5450f8d17d429fe2a3e6b2f2627a2
Stephen <s.cormican7@icloud.com>
2026-01-16 14:11:13 +0000

    update of functions

--- word_randomiser.py
+++ word_randomiser.py
@@ -3,34 +3,27 @@
 # This program gets the employee data from the user and
 # saves it as records in the employee.txt file
 
+def read_file():
+    pass
+
+
+def add_words():
+    pass
+
+
+def randomise_words():
+    pass
+
+
+def shuffle_words():
+    pass
+
+def save_file():
+    pass
+
+
 def main():
-    # Get the number of employee records to create
-    num_emps = int(input('How many employee records ' +
-                         'do you want to create? '))
+    pass
 
-    # Open the file for writing
-    emp_file = open('employees.txt', 'w')
-
-    # Get each employee's data and write it to the file
-    for count in range(1, num_emps + 1):
-        # Get the data for an employee
-        print(f'Enter data for employee #{count}')
-        name = input('Name: ')
-        id_num = input('ID number: ')
-        dept = input('Department: ')
-
-        # Write the data as a record to the file
-        emp_file.write(f'{name}\n')
-        emp_file.write(f'{id_num}\n')
-        emp_file.write(f'{dept}\n')
-
-        # Display a blank line
-        print()
-
-    # Close the file
-    emp_file.close()
-    print('Employee records written to employees.txt')
-
-# Call the main function
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
