0f102e69dc7d294c2cdd7dfaac82bc8a8a4438f3
Stephen <s.cormican7@icloud.com>
2026-05-05 13:03:39 +0100

    updated commented code in remove() function

--- word_randomiser.py
+++ word_randomiser.py
@@ -41,16 +41,14 @@ def display_data(word_list):
 
 
 def remove(word_list):
-    # pass word to be removed into parentheses as required
-    # still WIP as removal doesn't save
     # BE CAREFUL, .remove() removes first instance of item in the list
     x = input("What word, words, or phrase would you like to remove?: ")
     try:
         word_list.remove(x)
         print(f'\n{x} has been removed successfully.\n')
     except ValueError:
-        print(f'\n{x} is not a valid word in the list.\n'
-              f'Please try again.')
+        print(f'\n{x} has not been found in the list.\n'
+              f'Please try again.\n')
 
 def display_menu():
     print('\tMain Menu')
